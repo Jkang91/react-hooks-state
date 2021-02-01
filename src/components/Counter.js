84143f0e6d29393a953be7fdddec2dd15959ec01
jkang91 <jkang34@gmail.com>
2021-02-01 17:39:14 -0500

    hooks state example

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,10 +4,17 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
-    setCount(count + 1);
+    // console.log(`before setState: ${count}`)
+    setCount((currentCount) => currentCount + 1);
+    // setCount(count + 1);
+    // console.log(`after setState: ${count}`)
+    setCount((currentCount) => currentCount + 1);
+    // setCount(count + 1);
+    // console.log(`aftet setState twice: ${count}`)
   }
 
   return <button onClick={increment}>I have been clicked {count} times</button>;
+  return <div onClick={increment}>{count}</div>
 }
 
 export default Counter;
